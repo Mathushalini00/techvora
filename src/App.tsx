fb2a35dce15529511a228230680e3dd8743fe5b0
Mathushalini00 <153056389+Mathushalini00@users.noreply.github.com>
2026-01-16 21:19:46 +0530

    Add files via upload
    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Layout from "./app/layout";
+import HeroPage from "./templates/home/Hero";
+import Expertise from "./templates/home/Expertise";
+import Testimonials from "./templates/home/Clients";
+import FAQ from "./templates/home/Faq";
+
+function App() {
+  return (
+    <Layout>
+      <section id="home">
+        <HeroPage />
+      </section>
+
+      <section id="services">
+        <Expertise />
+      </section>
+
+      <section id="testimonials">
+        <Testimonials />
+      </section>
+
+      <section id="faq">
+        <FAQ />
+      </section>
+    </Layout>
+  );
+}
+
+export default App;
